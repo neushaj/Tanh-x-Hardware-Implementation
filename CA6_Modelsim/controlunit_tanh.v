f00ed8fe1c704355e8f253ab68b2b7c08a1dd50f
neushaj <neushajavidnia@ymail.com>
2022-06-27 17:57:49 +0430

    Indention Edited

--- CA6_Modelsim/controlunit_tanh.v
+++ CA6_Modelsim/controlunit_tanh.v
@@ -1,33 +1,38 @@
 `timescale 1ns/1ns
-module controlunit_tanh(input start,clk, rst,odd_even, Cout,output reg initz,
-Cen, Ld_term, Ld_expr, Ld_sqr,sel_x, sel_rom,
- sel_sqr, sel_term, sel_a, sel_pr,subsel,ready);
- reg [2:0] ps,ns;
-always@(posedge clk, posedge rst)
- if(rst) ps<= 3'b0;
- else ps<=ns;
-always@(ps,start,Cout)begin
-ns=3'b0;
-case(ps)
-  0:ns=(start==1)?3'b001:3'b0;
-  1:ns=3'b010;
-  2:ns=3'b011;
-  3:ns=3'b100;
-  4:ns=(Cout==1)?3'b0:3'b010;
-  default:ns=3'b0;
-endcase
-end
-always@(ps,odd_even)begin
-{initz,Cen,Ld_term, Ld_expr, Ld_sqr,sel_x, sel_rom,
- sel_sqr, sel_term, sel_a, sel_pr,subsel,ready}=13'b0;
- case(ps)
-   0:ready=1;
-   1:{Ld_sqr,sel_x,Ld_term,Ld_expr,initz}=5'b11111;
-   2:{sel_sqr,sel_term,sel_pr,Ld_term}=4'b1111;
-   3:{Ld_term,sel_pr,sel_rom,sel_term}=4'b1111;
-   4:begin {Ld_expr,sel_a,Cen}=3'b111; subsel=(odd_even==1)?1'b0:1'b1;end
-   default:{initz,Cen,Ld_term, Ld_expr, Ld_sqr,sel_x, sel_rom,
- sel_sqr, sel_term, sel_a, sel_pr,subsel,ready}=13'b0;
- endcase
- end
+module controlunit_tanh(input start, clk, rst, odd_even, Cout,
+                        output reg initz, Cen, Ld_term, Ld_expr,
+                        Ld_sqr,sel_x, sel_rom, sel_sqr, sel_term,
+                        sel_a, sel_pr,subsel,ready);
+    reg [2:0] ps,ns;
+    always@(posedge clk, posedge rst)
+        if(rst) ps<= 3'b0;
+        else ps<=ns;
+
+
+    always@(ps,start,Cout)begin
+        ns=3'b0;
+        case(ps)
+            0:ns=(start==1)?3'b001:3'b0;
+            1:ns=3'b010;
+            2:ns=3'b011;
+            3:ns=3'b100;
+            4:ns=(Cout==1)?3'b0:3'b010;
+            default:ns=3'b0;
+        endcase
+    end
+
+    always@(ps,odd_even)begin
+        {initz,Cen,Ld_term, Ld_expr, Ld_sqr,sel_x, sel_rom,
+        sel_sqr, sel_term, sel_a, sel_pr,subsel,ready}=13'b0;
+        case(ps)
+            0:ready=1;
+            1:{Ld_sqr,sel_x,Ld_term,Ld_expr,initz}=5'b11111;
+            2:{sel_sqr,sel_term,sel_pr,Ld_term}=4'b1111;
+            3:{Ld_term,sel_pr,sel_rom,sel_term}=4'b1111;
+            4:begin {Ld_expr,sel_a,Cen}=3'b111; subsel=(odd_even==1)?1'b0:1'b1;end
+            default:{initz,Cen,Ld_term, Ld_expr, Ld_sqr,sel_x, sel_rom,
+          sel_sqr, sel_term, sel_a, sel_pr,subsel,ready}=13'b0;
+        endcase
+    end
+
 endmodule
--- CA6_Modelsim/testbench_partD.v
+++ CA6_Modelsim/testbench_partD.v
@@ -1,27 +1,32 @@
 `timescale 1ns/1ns
 module PartD_TB();
-reg start=0,clock=0,reset=0;
-reg [15:0]X;
-wire [15:0]Y;
-wire ready;
-tanh_modelsim MUT(X,start,clock,reset,ready,Y);
-initial begin
-  #10 reset=1;
-  #20 reset=0;
-  #20 start=1;
-  #50 X=16'b 0100000000000000;
-  #10 start=0;
-  #3000 X=16'b 0001000100010001;
-  #10 start=1;
-  #50 start=0;
-  #3000 X=16'b 0000010000100010;
-  #10 start=1;
-  #50 start=0;
-  #20000 $stop;
-end
-initial begin
-  #70 clock=1;
-  repeat(800) #30 clock=~clock;
-  #20 $stop;
-end
+
+    reg start=0,clock=0,reset=0;
+    reg [15:0]X;
+    wire [15:0]Y;
+    wire ready;
+
+    tanh_modelsim MUT(X,start,clock,reset,ready,Y);
+
+    initial begin
+        #10 reset=1;
+        #20 reset=0;
+        #20 start=1;
+        #50 X=16'b 0100000000000000;
+        #10 start=0;
+        #3000 X=16'b 0001000100010001;
+        #10 start=1;
+        #50 start=0;
+        #3000 X=16'b 0000010000100010;
+        #10 start=1;
+        #50 start=0;
+        #20000 $stop;
+    end
+
+    initial begin
+        #70 clock=1;
+        repeat(800) #30 clock=~clock;
+        #20 $stop;
+    end
+    
 endmodule
